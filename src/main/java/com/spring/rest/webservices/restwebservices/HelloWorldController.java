9bd4d58b8dfadfbeb6354962a86d0011dccf50c7
Rebelo <Luis_Rebelo@Dellteam.com>
2019-11-06 17:36:54 -0300

    Hellow world path variable method

--- src/main/java/com/spring/rest/webservices/restwebservices/HelloWorldController.java
+++ src/main/java/com/spring/rest/webservices/restwebservices/HelloWorldController.java
@@ -1,21 +1,26 @@
 package com.spring.rest.webservices.restwebservices;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Handle with HTTP requests and REST requests
 @RestController
 public class HelloWorldController {
 
+    //Map returning string
     @GetMapping("/helloWorld")
     public String helloWorld(){
         return "Hello World!";
     }
 
+    //Map returning a bean
     @GetMapping("/helloWorldBean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello world! (w/ Bean)");
+    }
+
+    //Map returning a bean w/ a variable
+    @GetMapping("/helloWorld/pathVariable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean("Hello, " + name);
     }
 }
