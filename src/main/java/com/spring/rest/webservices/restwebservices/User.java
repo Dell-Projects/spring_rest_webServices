9cba3b7e549ad2b0aa79aa3e03aefa1896b8309d
Rebelo <Luis_Rebelo@Dellteam.com>
2019-11-08 18:45:59 -0300

    User entity

--- src/main/java/com/spring/rest/webservices/restwebservices/User.java
+++ src/main/java/com/spring/rest/webservices/restwebservices/User.java
@@ -2,12 +2,18 @@ package com.spring.rest.webservices.restwebservices;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Entity
 public class User extends RepresentationModel<User> {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message = "Name should have at least 2 characters")
     private String name;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (1, sysdate(), 'Roberto');
+insert into user values (2, sysdate(), 'Daniela');
+insert into user values (3, sysdate(), 'Arthur');
